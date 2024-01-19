92e7b5d2f92f4592b655834fde4280c7c5e84a84
GuiTaek <109903402+GuiTaek@users.noreply.github.com>
2024-01-19 22:15:17 +0100

    remove fog applier from client side
    the only reason I included it in client side is, that it didn't worked on server side
    now that I know why it didn't work, I can safely delete client side

--- src/main/java/com/gmail/guitaekm/technoenderling/event/ModEventsClient.java
+++ src/main/java/com/gmail/guitaekm/technoenderling/event/ModEventsClient.java
@@ -4,6 +4,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class ModEventsClient {
     public static void registerEvents() {
-        ClientTickEvents.END_CLIENT_TICK.register(new DarknessApplierClient());
+        
     }
 }
