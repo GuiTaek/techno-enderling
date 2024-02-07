f4e3fdb150c12950a397d532aa1ef2a9b2cd8252
GuiTaek <109903402+GuiTaek@users.noreply.github.com>
2024-02-07 22:16:47 +0100

    adjust different worldgen frequencies

--- src/main/java/com/gmail/guitaekm/technoenderling/worldgen/ModStructureFeatures.java
+++ src/main/java/com/gmail/guitaekm/technoenderling/worldgen/ModStructureFeatures.java
@@ -19,7 +19,8 @@ public class ModStructureFeatures {
      * It is always a good idea to register your Structures so that other mods and datapacks can
      * use them too directly from the registries. It great for mod/datapacks compatibility.
      */
-    public static StructureFeature<StructurePoolFeatureConfig> POCKET_PORTAL = new PocketPortalStructure(StructurePoolFeatureConfig.CODEC);
+    public static StructureFeature<StructurePoolFeatureConfig> COMMON_POCKET_PORTAL = new PocketPortalStructure(StructurePoolFeatureConfig.CODEC);
+    public static StructureFeature<StructurePoolFeatureConfig> RARE_POCKET_PORTAL = new PocketPortalStructure(StructurePoolFeatureConfig.CODEC);
 
     /**
      * This is where we use Fabric API's structure API to setup the StructureFeature
@@ -30,15 +31,45 @@ public class ModStructureFeatures {
         // This is Fabric API's builder for structures.
         // It has many options to make sure your structure will spawn and work properly.
         // Give it your structure and the identifier you want for it.
-        FabricStructureBuilder.create(new Identifier(TechnoEnderling.MOD_ID, "pocket_portal"), POCKET_PORTAL)
+        FabricStructureBuilder.create(new Identifier(TechnoEnderling.MOD_ID, "common_pocket_portal"), COMMON_POCKET_PORTAL)
 
                 /* Generation stage for when to generate the structure. there are 10 stages you can pick from!
                    This surface structure stage places the structure before plants and ores are generated. */
                 .step(GenerationStep.Feature.SURFACE_STRUCTURES)
 
                 .defaultConfig(new StructureConfig(
-                        1, /* average distance apart in chunks between spawn attempts */
-                        0, /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE */
+                        3, /* average distance apart in chunks between spawn attempts */
+                        2, /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE */
+                        399117345 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */))
+
+                /*
+                 * Whether surrounding land will be modified automatically to conform to the bottom of the structure.
+                 * Basically, it adds land at the base of the structure like it does for Villages and Outposts.
+                 * Doesn't work well on structure that have pieces stacked vertically or change in heights.
+                 *
+                 * Note: The air space this method will create will be filled with water if the structure is below sealevel.
+                 * This means this is best for structure above sealevel so keep that in mind.
+                 */
+                .adjustsSurface()
+
+                /* Finally! Now we register our structure and everything above will take effect. */
+                .register();
+
+
+
+        // Add more structures here and so on
+        // This is Fabric API's builder for structures.
+        // It has many options to make sure your structure will spawn and work properly.
+        // Give it your structure and the identifier you want for it.
+        FabricStructureBuilder.create(new Identifier(TechnoEnderling.MOD_ID, "rare_pocket_portal"), RARE_POCKET_PORTAL)
+
+                /* Generation stage for when to generate the structure. there are 10 stages you can pick from!
+                   This surface structure stage places the structure before plants and ores are generated. */
+                .step(GenerationStep.Feature.SURFACE_STRUCTURES)
+
+                .defaultConfig(new StructureConfig(
+                        17, /* average distance apart in chunks between spawn attempts */
+                        12, /* minimum distance apart in chunks between spawn attempts. MUST BE LESS THAN ABOVE VALUE */
                         399117345 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */))
 
                 /*
--- src/main/java/com/gmail/guitaekm/technoenderling/worldgen/RegisterModStructures.java
+++ src/main/java/com/gmail/guitaekm/technoenderling/worldgen/RegisterModStructures.java
@@ -2,11 +2,23 @@ package com.gmail.guitaekm.technoenderling.worldgen;
 
 import com.gmail.guitaekm.technoenderling.TechnoEnderling;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 
+import java.util.List;
+
 public class RegisterModStructures {
+
+    public static List<Identifier> rarePocketPortalBiomes = List.of(
+            new Identifier(TechnoEnderling.MOD_ID, "enderworld_wastes"),
+            new Identifier(TechnoEnderling.MOD_ID, "enderworld_forest")
+    );
+
+    public static List<Identifier> commonPocketPortalBiomes = List.of(
+            new Identifier(TechnoEnderling.MOD_ID, "enderworld_foggy_forest")
+    );
 
     public static void register() {
 
@@ -24,6 +36,23 @@ public class RegisterModStructures {
      * Just make sure you call BiomeModifications.addStructure at mod init.
      */
     public static void addToBiomes() {
+        /*
+         * This is the API you will use to add anything to any biome.
+         * This includes spawns, changing the biome's looks, messing with its temperature,
+         * adding carvers, spawning new features... etc
+         */
+        BiomeModifications.addStructure(
+                // Add our structure to all biomes that have any of these biome categories. This includes modded biomes.
+                // You can filter to certain biomes based on stuff like temperature, scale, precipitation, mod id, etc.
+                // See BiomeSelectors's methods for more options or write your own by doing `(context) -> context.whatever() == condition`
+                context -> isInBiomeList(context, RegisterModStructures.commonPocketPortalBiomes),
+                // The registry key of our ConfiguredStructure so BiomeModification API can grab it
+                // later to tell the game which biomes that your structure can spawn within.
+                RegistryKey.of(
+                        Registry.CONFIGURED_STRUCTURE_FEATURE_KEY,
+                        new Identifier(TechnoEnderling.MOD_ID, "common_pocket_portal")
+                )
+        );
 
         /*
          * This is the API you will use to add anything to any biome.
@@ -34,13 +63,26 @@ public class RegisterModStructures {
                 // Add our structure to all biomes that have any of these biome categories. This includes modded biomes.
                 // You can filter to certain biomes based on stuff like temperature, scale, precipitation, mod id, etc.
                 // See BiomeSelectors's methods for more options or write your own by doing `(context) -> context.whatever() == condition`
-                biomeSelectionContext -> biomeSelectionContext.getBiomeKey().getValue().equals(new Identifier(TechnoEnderling.MOD_ID, "enderworld_wastes")),
+                context -> isInBiomeList(context, RegisterModStructures.rarePocketPortalBiomes),
                 // The registry key of our ConfiguredStructure so BiomeModification API can grab it
                 // later to tell the game which biomes that your structure can spawn within.
                 RegistryKey.of(
                         Registry.CONFIGURED_STRUCTURE_FEATURE_KEY,
-                        new Identifier(TechnoEnderling.MOD_ID, "pocket_portal")
+                        new Identifier(TechnoEnderling.MOD_ID, "rare_pocket_portal")
                 )
         );
+    }
+
+    public static boolean isInBiomeList(BiomeSelectionContext context, List<Identifier> biomeList) {
+        for (Identifier biome : biomeList) {
+            if (context
+                    .getBiomeKey()
+                    .getValue()
+                    .equals(biome)
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
