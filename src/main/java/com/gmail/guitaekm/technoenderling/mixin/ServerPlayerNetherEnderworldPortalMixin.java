4d60eb5614fa338a3545a42d1964ecc59c3960fc
GuiTaek <109903402+GuiTaek@users.noreply.github.com>
2024-02-22 19:28:14 +0100

    make nether destinations persistant

--- src/main/java/com/gmail/guitaekm/technoenderling/mixin/ServerPlayerNetherEnderworldPortalMixin.java
+++ src/main/java/com/gmail/guitaekm/technoenderling/mixin/ServerPlayerNetherEnderworldPortalMixin.java
@@ -2,13 +2,20 @@ package com.gmail.guitaekm.technoenderling.mixin;
 
 import com.gmail.guitaekm.technoenderling.access.IServerPlayerNetherEnderworldPortal;
 import com.gmail.guitaekm.technoenderling.blocks.EnderworldPortalBlock;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -103,5 +110,57 @@ public class ServerPlayerNetherEnderworldPortalMixin implements IServerPlayerNet
     @Override
     public @Nullable EnderworldPortalBlock.NetherInstance techno_enderling$getSource() {
         return this.source;
+    }
+
+    // todo: probably needs to be in a util class
+    private static void putPos(String name, NbtCompound nbt, BlockPos pos) {
+        NbtCompound posNbt = new NbtCompound();
+        posNbt.putInt("x", pos.getX());
+        posNbt.putInt("y", pos.getY());
+        posNbt.putInt("z", pos.getZ());
+        nbt.put(name, posNbt);
+    }
+
+    // todo: probably needs to be in a util class
+    private static BlockPos getPos(String name, NbtCompound nbt) {
+        NbtCompound nbtPos = nbt.getCompound(name);
+        return new BlockPos(nbtPos.getInt("x"), nbtPos.getInt("y"), nbtPos.getInt("z"));
+    }
+
+    // todo: probably needs to be in a util class
+    private static NbtCompound instanceToNbt(EnderworldPortalBlock.NetherInstance instance) {
+        NbtCompound nbtInstance = new NbtCompound();
+        nbtInstance.putString("name", instance.name());
+        putPos("pos", nbtInstance, instance.pos());
+        nbtInstance.putInt("id", instance.id());
+        return nbtInstance;
+    }
+
+    // todo: probably needs to be in a util class
+    private static EnderworldPortalBlock.NetherInstance nbtToInstance(NbtCompound nbtInstance) {
+        String instanceName = nbtInstance.getString("name");
+        BlockPos pos = getPos("pos", nbtInstance);
+        int id = nbtInstance.getInt("id");
+        return new EnderworldPortalBlock.NetherInstance(id, instanceName, pos);
+    }
+    @Inject(method = "writeCustomDataToNbt", at=@At("TAIL"))
+    public void writeCustomDataToNbtTail(NbtCompound nbt, CallbackInfo ci) {
+        NbtList nbtDestinations = new NbtList();
+        this.savedDestinations.forEach(
+                netherInstance -> nbtDestinations.add(instanceToNbt(netherInstance))
+        );
+        nbt.put("netherDestinations", nbtDestinations);
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at=@At("TAIL"))
+    public void readCustomDataFromNbtTail(NbtCompound nbt, CallbackInfo ci) {
+        this.savedDestinations.clear();
+        NbtList nbtDesinations = nbt.getList("netherDestinations", NbtElement.COMPOUND_TYPE);
+        this.savedDestinations.addAll(
+                nbtDesinations
+                        .stream()
+                        .map(nbtElement -> nbtToInstance((NbtCompound) nbtElement))
+                        .toList()
+        );
     }
 }
